5e2f6759b3d3d48ecb4fdad89a9ee7bb26346ec4
Rodrigo Andrade <rodrigom.andrade95@gmail.com>
2022-04-20 11:57:04 -0300

    Started identity implementation

---
+++ UsuariosApi/Controllers/CadastroController.cs
@@ -0,0 +1,30 @@
+﻿using FluentResults;
+using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Data.Dtos;
+using UsuariosApi.Services;
+
+namespace UsuariosApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CadastroController : ControllerBase
+    {
+        private readonly CadastroService _cadastroService;
+
+        public CadastroController(CadastroService cadastroService)
+        {
+            _cadastroService = cadastroService;
+        }
+
+        [HttpPost]
+        public IActionResult CadastraUsuario(CreateUsuarioDto createDto)
+        {
+            Result resultado = _cadastroService.CadastraUsuario(createDto);
+
+            if (resultado.IsFailed)
+                return StatusCode(500);
+
+            return Ok();
+        }
+    }
+}
